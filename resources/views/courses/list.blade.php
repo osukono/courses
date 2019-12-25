1d98314455725def081c8110be258542fc21dd99
Andrew Sukonchenko <osukono@gmail.com>
2019-12-25 15:10:02 +0200

    Index

--- resources/views/courses/list.blade.php
+++ resources/views/courses/list.blade.php
@@ -7,10 +7,25 @@
                     @if($course->free)
                         <span class="badge badge-success ml-2">{{ __('Free') }}</span>
                     @endif
+                    <a href="{{ route('courses.practice', $course) }}" class="btn btn-outline-primary ml-2">{{ __('Practice') }}</a>
                 </h5>
             </div>
-            <p class="card-text">{{ $course->description }}</p>
-            <a href="{{ route('courses.practice', $course) }}" class="btn btn-outline-primary">{{ __('Practice') }}</a>
+            <p class="card-text mx-3">{{ $course->description }}</p>
+            <div class="ml-3">
+            <h5>{{ __('Course topics') }}</h5>
+            <table class="table table-sm table-borderless ml-3">
+                <tbody>
+                @foreach($course->latestContent->courseLessons as $courseLesson)
+                    <tr>
+                        <td class="text-right">{{ $courseLesson->number . '. ' }}</td>
+                        <td class="col-11">
+                            {{ $courseLesson->title }}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            </div>
         </div>
     </div>
 @endforeach
