91bb0c747ac259c1b272aa26f000b9b5b8c380c1
Andrew Sukonchenko <osukono@gmail.com>
2019-12-29 20:13:07 +0200

    Course description

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -11,15 +11,15 @@
             </h5>
         </div>
         <p class="card-text mx-3" lang="{{ $course->translation->code }}">{{ $course->description }}</p>
-        {{--<h5>{{ __('Course topics') }}</h5>
+        <h5>{{ __('Course topics') }}</h5>
         <div class="row ml-3">
             @foreach($course->latestContent->courseLessons->chunk(ceil($course->latestContent->courseLessons->count() / 2)) as $chunk)
-                <div class="col-12 col-md-6 px-0">
-                    <table class="table table-sm table-borderless">
+                <div class="col-12 col-lg-4 col-md-6">
+                    <table class="table table-sm table-borderless mb-0">
                         <tbody>
                         @foreach($chunk as $courseLesson)
                             <tr>
-                                <td class="text-right">{{ $courseLesson->number . '. ' }}</td>
+                                <td class="text-right" style="min-width: 30px">{{ $courseLesson->number . '. ' }}</td>
                                 <td class="col-11">
                                     <a href="{{ route('courses.show.lesson', [$course, $courseLesson->number]) }}">{{ $courseLesson->title }}</a>
                                 </td>
@@ -29,8 +29,8 @@
                     </table>
                 </div>
             @endforeach
-        </div>--}}
-        <div class="ml-3">
+        </div>
+        {{--<div class="ml-3">
             <h5>{{ __('Course topics') }}</h5>
             <table class="table table-sm table-borderless ml-3">
                 <tbody>
@@ -44,6 +44,6 @@
                 @endforeach
                 </tbody>
             </table>
-        </div>
+        </div>--}}
     </div>
 </div>
