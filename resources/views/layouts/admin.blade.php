0e598b042f493802cfb52a7580faf4e7be7d2a9b
Andrew Sukonchenko <osukono@gmail.com>
2020-02-10 10:08:19 +0200

    favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,9 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
     <script src="{{ mix('js/admin.js') }}"></script>
     <script src="{{ mix('js/vue.js') }}" async defer></script>
