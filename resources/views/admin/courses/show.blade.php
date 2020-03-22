95b148e8de78d1640fabf0acfb5b5456f204fe15
Andrew Sukonchenko <osukono@gmail.com>
2020-03-22 16:37:56 +0200

    Editors

--- resources/views/admin/courses/show.blade.php
+++ resources/views/admin/courses/show.blade.php
@@ -108,6 +108,7 @@
 
     <div class="card shadow-sm">
         <div class="card-body">
+            <h5 class="card-title mb-4">{{ $course }}</h5>
             @include('admin.courses.lessons')
         </div>
     </div>
