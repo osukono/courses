3dc8dbd45c3c3f82f2c6ea29a22c819b11167757
Andrew Sukonchenko <osukono@gmail.com>
2020-03-22 18:44:02 +0200

    Editors

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -8,6 +8,7 @@
     @if($courses->count())
         <div class="card shadow-sm">
             <div class="card-body">
+                <h5 class="card-title mb-4">Courses</h5>
                 @include('admin.courses.list')
             </div>
         </div>
