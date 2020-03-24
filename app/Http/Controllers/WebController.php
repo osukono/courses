669b580ae99dd49d12bfde19c4d0117bb7af76a8
Andrew Sukonchenko <osukono@gmail.com>
2020-03-24 13:26:06 +0200

    Course list

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -10,8 +10,8 @@ class WebController extends Controller
     public function index()
     {
         $data['courses'] = CourseRepository::all()
-            ->ordered()->get()
-            ->groupBy(['language_id', 'level_id', 'topic_id'])->first();
+            ->ordered()->get();
+//            ->groupBy(['language_id', 'level_id', 'topic_id'])->first();
 
         $data['seo']['title'] = __('web.index.seo.title');
         $data['seo']['description'] = __('web.index.seo.description');
