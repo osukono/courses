a13a08e6bcbc9052e295719d05df16b89f0f2447
Andrew Sukonchenko <osukono@gmail.com>
2020-04-01 20:44:20 +0300

    Console

--- resources/views/admin/courses/list.blade.php
+++ resources/views/admin/courses/list.blade.php
@@ -1,26 +1,26 @@
 <table class="table">
     <thead>
     <tr>
-        <th class="col-7"></th>
-        <th>Lessons</th>
-        <th class="text-nowrap">Player</th>
-        <th class="text-nowrap text-right">Committed</th>
-        <th class="text-nowrap text-right">Status</th>
-        <th class="text-nowrap">Firebase ID</th>
+        <th class="col-12 col-md-7"></th>
+        <th class="d-none d-md-table-cell">Lessons</th>
+        <th class="d-none d-md-table-cell">Player</th>
+        <th class="text-right d-none d-md-table-cell">Committed</th>
+        <th class="text-right d-none d-md-table-cell">Status</th>
+        <th class="text-nowrap d-none d-md-table-cell">Firebase ID</th>
     </tr>
     </thead>
     <tbody>
     @foreach($courses as $course)
         <tr class="clickable-row" data-href="{{ route('admin.courses.show', $course) }}">
-            <td class="text-nowrap">{{ $course }}</td>
-            <td>{{ $course->course_lessons_count }}</td>
-            <td>{{ $course->player_version }}</td>
-            <td class="text-nowrap text-right">
+            <td>{{ $course }}</td>
+            <td class="d-none d-md-table-cell">{{ $course->course_lessons_count }}</td>
+            <td class="d-none d-md-table-cell">{{ $course->player_version }}</td>
+            <td class="text-nowrap text-right d-none d-md-table-cell">
                 @isset($course->committed_at)
                     {{ $course->committed_at->diffForHumans() }}
                 @endisset
             </td>
-            <td class="text-nowrap text-right">
+            <td class="text-nowrap text-right d-none d-md-table-cell">
                 @isset($course->firebase_id)
                     @if($course->is_updating)
                         Updating
@@ -33,7 +33,7 @@
                     New
                 @endisset
             </td>
-            <td>
+            <td class="d-none d-md-table-cell">
                 {{ $course->firebase_id }}
             </td>
         </tr>
