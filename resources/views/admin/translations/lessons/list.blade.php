05f87c16ae2e7271dd446ab5aad56631ef203ed4
Andrew Sukonchenko <osukono@gmail.com>
2020-04-01 19:58:16 +0300

    Console

--- resources/views/admin/translations/lessons/list.blade.php
+++ resources/views/admin/translations/lessons/list.blade.php
@@ -2,16 +2,16 @@
     <thead>
     <tr>
         <th></th>
-        <th class="col-9"></th>
-        <th>Exercises</th>
-        <th class="text-right text-nowrap">Last Modified</th>
+        <th class="col-12 col-md-9"></th>
+        <th class="d-none d-md-table-cell">Exercises</th>
+        <th class="text-right text-nowrap d-none d-md-table-cell">Last Modified</th>
     </tr>
     </thead>
     <tbody>
     @foreach($lessons as $lesson)
         <tr>
             <td>{{ $lesson->index }}</td>
-            <td class="text-nowrap clickable-row"
+            <td class="clickable-row"
                 data-href="{{ route('admin.translations.lesson.show', [$language, $lesson]) }}">
                 @if($lesson->isDisabled($content->language))
                     <span class="badge badge-warning text-uppercase">Disabled</span>
@@ -21,8 +21,8 @@
                 @endif
                 {{ $lesson }}
             </td>
-            <td>{{ $lesson->exercises_count }}</td>
-            <td class="text-nowrap text-right">{{ $lesson->updated_at->diffForHumans() }}</td>
+            <td class="d-none d-md-table-cell d-none d-md-table-cell">{{ $lesson->exercises_count }}</td>
+            <td class="text-nowrap text-right d-none d-md-table-cell">{{ $lesson->updated_at->diffForHumans() }}</td>
         </tr>
     @endforeach
     </tbody>
