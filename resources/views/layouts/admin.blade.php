01fdcce65ced679cae9541167543bd067ebddb58
Andrew Sukonchenko <osukono@gmail.com>
2020-04-13 15:32:02 +0300

    Console

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,7 @@
     <nav id="sidebar" {{--class="border-right border-light"--}}>
         <div class="sidebar-header text-center mt-2">
             <a href="{{ route('welcome') }}"><h3 class="brand">{{ env('APP_NAME') }}</h3></a>
+            {{ Auth::getUser()->name }}
         </div>
 
         <ul class="list-unstyled">
