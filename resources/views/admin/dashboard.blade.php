187acb2efa4a031c0c3bd0abd6f01ce3aa85c5eb
Andrew Sukonchenko <osukono@gmail.com>
2020-11-22 14:47:59 +0200

    Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <div class="card shadow-sm">
                 <div class="card-body">
-                    <h5 class="card-title mb-4">Users</h5>
+                    <h5 class="card-title mb-4">Application Users</h5>
                     <div class="h3">{{ $users_count }}</div>
                 </div>
             </div>
@@ -17,7 +17,7 @@
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <div class="card shadow-sm">
                 <div class="card-body">
-                    <h5 class="card-title mb-4">Courses</h5>
+                    <h5 class="card-title mb-4">Published Courses</h5>
                     <div class="h3">{{ $courses_count }}</div>
                 </div>
             </div>
