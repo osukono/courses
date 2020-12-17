f02c90a42bf90efdd70aa5ca4312522c4d8f0210
Andrew Sukonchenko <osukono@gmail.com>
2020-12-17 15:32:23 +0200

    -a

--- resources/views/index/promo.blade.php
+++ resources/views/index/promo.blade.php
@@ -3,7 +3,7 @@
         <div class="row py-3">
             <div class="col-12 col-md-8 text-center align-self-center">
                 <h4 class="text-primary mb-0">
-                    {{ __('web.index.section.promo.text', ['course' => $courses->first()->language->name . ' ' . $courses->first()->level->name]) }}
+                    {{ __('web.index.section.promo.text', ['course' => $courses->first()->language->native . ' › ' . $courses->first()->title]) }}
                 </h4>
             </div>
             <div class="col-12 text-center pt-3 pl-0 pr-0 col-md-4 pt-md-0 pr-md-5">
