2f93efa497d96cc767f20718a451ec4d05248ad3
Andrew Sukonchenko <osukono@gmail.com>
2020-12-18 14:48:27 +0200

    buttons

--- resources/views/documents/privacy.blade.php
+++ resources/views/documents/privacy.blade.php
@@ -13,9 +13,6 @@
         <p>
             If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
         </p>
-        {{--<p>
-            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Yummy Lingo unless otherwise defined in this Privacy Policy.
-        </p>--}}
 
         <hr class="my-5">
 
