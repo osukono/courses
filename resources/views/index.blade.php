7110061cbdecd10bd4242e04fd82fa3ff30d391e
Andrew Sukonchenko <osukono@gmail.com>
2020-12-20 16:49:16 +0200

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @include('components.header', [
-    'header_img' => URL::asset('images/index.svg?v=12'),
+    'header_img' => URL::asset('images/index.svg?v=14'),
     'header_img_sm' => URL::asset('images/index_sm.svg?v=4'),
     'header_title' => __('web.index.section.top.header'),
     'header_text' => __('web.index.section.top.text'),
