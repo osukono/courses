a763b4182271da12cbfbda2c1ea6db8548e0cc11
Andrew Sukonchenko <osukono@gmail.com>
2021-01-05 17:43:20 +0200

    admin

--- resources/views/html/form/input.blade.php
+++ resources/views/html/form/input.blade.php
@@ -1,19 +1,23 @@
 <div class="form-group">
+    @php
+        /** @var string $name */
+        $arrDotName = Str::of($name)->replace('[', '.')->replace(']', '')->__toString()
+    @endphp
     @isset($label)<label for="{{ $name }}">{{ $label }}</label>@endisset
     <input type="{{ isset($type) ? $type : 'text' }}"
-           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error(Str::of($name)->replace('[', '.')->replace(']', '')->__toString()) is-invalid @enderror"
+           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error($arrDotName) is-invalid @enderror"
            id="{{ $name }}" name="{{ $name }}" @isset($label) placeholder="{{ $label }}" @endisset
            @isset($lang) lang="{{ $lang }}" @endisset
-           value="{{ isset($default) ? old(Str::of($name)->replace('[', '.')->replace(']', '')->__toString(), $default) : old(Str::of($name)->replace('[', '.')->replace(']', '')->__toString()) }}"
+           value="{{ isset($default) ? old($arrDotName, $default) : old($arrDotName) }}"
         {{ !empty($autofocus) ? ' autofocus' : '' }}
         {{ !empty($required) ? ' required' : '' }}
     >
     @isset($helper)
         <small class="form-text text-muted">{{ $helper }}</small>
     @endisset
-    @error($name)
+    @error($arrDotName)
     <div class="invalid-feedback">
-        {{ $errors->first(Str::of($name)->replace('[', '.')->replace(']', '')->__toString()) }}
+        {{ $errors->first($arrDotName) }}
     </div>
     @enderror
 </div>
