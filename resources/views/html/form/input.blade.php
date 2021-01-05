f7ccd830b01721d65550ae40e95a6aeee7f611e5
Andrew Sukonchenko <osukono@gmail.com>
2021-01-05 17:23:19 +0200

    index

--- resources/views/html/form/input.blade.php
+++ resources/views/html/form/input.blade.php
@@ -1,10 +1,10 @@
 <div class="form-group">
     @isset($label)<label for="{{ $name }}">{{ $label }}</label>@endisset
     <input type="{{ isset($type) ? $type : 'text' }}"
-           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error($name) is-invalid @enderror"
-           id="{{ $name }}" name="{{ Str::of($name)->replace('[', '.')->replace(']', '') }}" @isset($label) placeholder="{{ $label }}" @endisset
+           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error(Str::of($name)->replace('[', '.')->replace(']', '')) is-invalid @enderror"
+           id="{{ $name }}" name="{{ $name }}" @isset($label) placeholder="{{ $label }}" @endisset
            @isset($lang) lang="{{ $lang }}" @endisset
-           value="{{ isset($default) ? old($name, $default) : old($name) }}"
+           value="{{ isset($default) ? old(Str::of($name)->replace('[', '.')->replace(']', ''), $default) : old(Str::of($name)->replace('[', '.')->replace(']', '')) }}"
         {{ !empty($autofocus) ? ' autofocus' : '' }}
         {{ !empty($required) ? ' required' : '' }}
     >
