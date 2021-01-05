a2211f853d97fc6a65a98c16f530bfdbfa6ad39b
Andrew Sukonchenko <osukono@gmail.com>
2021-01-05 17:34:38 +0200

    admin

--- resources/views/html/form/input.blade.php
+++ resources/views/html/form/input.blade.php
@@ -1,10 +1,10 @@
 <div class="form-group">
     @isset($label)<label for="{{ $name }}">{{ $label }}</label>@endisset
     <input type="{{ isset($type) ? $type : 'text' }}"
-           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error(Str::of($name)->replace('[', '.')->replace(']', '')) is-invalid @enderror"
+           class="form-control {{ !empty($lg) ? 'form-control-lg' : '' }} @error(Str::of($name)->replace('[', '.')->replace(']', '')->__toString()) is-invalid @enderror"
            id="{{ $name }}" name="{{ $name }}" @isset($label) placeholder="{{ $label }}" @endisset
            @isset($lang) lang="{{ $lang }}" @endisset
-           value="{{ isset($default) ? old(Str::of($name)->replace('[', '.')->replace(']', ''), $default) : old(Str::of($name)->replace('[', '.')->replace(']', '')) }}"
+           value="{{ isset($default) ? old(Str::of($name)->replace('[', '.')->replace(']', '')->__toString(), $default) : old(Str::of($name)->replace('[', '.')->replace(']', '')->__toString()) }}"
         {{ !empty($autofocus) ? ' autofocus' : '' }}
         {{ !empty($required) ? ' required' : '' }}
     >
