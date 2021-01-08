aa0c18c7399271047ef702526c3ae975ad8c7492
Andrew Sukonchenko <osukono@gmail.com>
2021-01-08 14:55:06 +0200

    dashboard

---
+++ resources/views/admin/card.blade.php
@@ -0,0 +1,10 @@
+<div class="col-auto mb-4 px-0 mx-3" style="min-width: 300px">
+    <div class="card shadow-sm">
+        <div class="card-header">
+            {{ $title }}
+        </div>
+        <div class="card-body">
+            <h3>{{ $body }}</h3>
+        </div>
+    </div>
+</div>
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,45 +6,9 @@
 
 @section('content')
     <div class="row">
-        <div class="col-auto mb-4 px-0 mx-3" style="min-width: 300px">
-            <div class="card shadow-sm">
-                <div class="card-header">
-                    Users
-                </div>
-                <div class="card-body">
-                    <h3>{{ $users_count }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto mb-4 px-0 mx-3" style="min-width: 300px">
-            <div class="card shadow-sm">
-                <div class="card-header">
-                    Courses
-                </div>
-                <div class="card-body">
-                    <h3>{{ $courses_count }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto mb-4 px-0 mx-3" style="min-width: 300px">
-            <div class="card shadow-sm">
-                <div class="card-header">
-                    Lessons Learned
-                </div>
-                <div class="card-body">
-                    <h3>{{ $lessons_learned }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto mb-4 px-0 mx-3" style="min-width: 300px">
-            <div class="card shadow-sm">
-                <div class="card-header">
-                    Development Activity (last month)
-                </div>
-                <div class="card-body">
-                    <h3>{{ $devActivity }} changes</h3>
-                </div>
-            </div>
-        </div>
+        @include('admin.card', ['title' => 'Users', 'body' => $users_count])
+        @include('admin.card', ['title' => 'Courses', 'body' => $courses_count])
+        @include('admin.card', ['title' => 'Lessons Learned', 'body' => $lessons_learned])
+        @include('admin.card', ['title' => 'Development Activity (last month)', 'body' => $devActivity . ' changes'])
     </div>
 @endsection
