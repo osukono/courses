b768f6449143cb5a0378a66b06ca6a88fabb945f
Andrew Sukonchenko <osukono@gmail.com>
2021-01-08 19:00:03 +0200

    images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     @include('components.header', [
-    'header_img' => URL::asset('images/index.svg?v=15'),
-    'header_img_sm' => URL::asset('images/index_sm.svg?v=5'),
+    'header_img' => URL::asset('images/index_' . LaravelLocalization::getCurrentLocale()  . '.svg?v=17'),
+    'header_img_sm' => URL::asset('images/index_sm_' . LaravelLocalization::getCurrentLocale()  . '.svg?v=6'),
     'header_title' => __('web.index.section.top.header'),
     'header_text' => __('web.index.section.top.text'),
     'header_button_link' => '#apps',
