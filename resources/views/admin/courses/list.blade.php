975f0ed08b287344ccde400d5791a5c2de4cdc1f
Andrew Sukonchenko <osukono@gmail.com>
2021-01-21 12:35:39 +0200

    Commit

--- resources/views/admin/courses/list.blade.php
+++ resources/views/admin/courses/list.blade.php
@@ -22,7 +22,7 @@
                     @endisset
                     <div class="col">
                         {{ $course }}
-                        <div class=" text-muted small" style="display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;">
+                        <div class=" text-muted small" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;">
                             {{ $course->description }}
                         </div>
                     </div>
