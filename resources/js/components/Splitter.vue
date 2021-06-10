e870751fe3de1d2d193d2188332a3b669f646b95
Andrew Sukonchenko <osukono@gmail.com>
2021-06-10 06:49:36 +0300

    Commit

--- resources/js/components/Splitter.vue
+++ resources/js/components/Splitter.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
-    <h5><span class="badge rounded-pill bg-light text-secondary mr-2 px-2" v-for="part in parts">{{ part }}</span></h5>
+    <h5>
+        <span class="badge rounded-pill bg-light text-secondary mr-2 px-2"
+              v-for="part in parts">
+            {{ part }}
+        </span>
+    </h5>
 </div>
 </template>
 
@@ -24,9 +29,7 @@ export default {
         this.value = this.value.replaceAll('\[', '');
         this.value = this.value.replaceAll(']', '');
 
-        // this.parts = this.value.split(/ /gi);
-
-        this.parts = this.value.split(/(?<!-|(^| )(in|at|on|to)|(^| )(my|our|his|her|its|their|your)|^the| the|^a| a|^an| an|\d+) /gi);
+        this.parts = this.value.split(/(?<!-|(^| )(in|at|on(?! the | a | an )|to)|(^| )(my|our|his|her|its|their|your)|^the| the|^a| a|^an| an|\d+) /gi);
     },
 }
 </script>
