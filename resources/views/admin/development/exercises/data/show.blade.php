0fb3be5951c5d40d445d2ab65e02b4cfffcfe52f
Andrew Sukonchenko <osukono@gmail.com>
2021-06-26 20:51:12 +0300

    Commit

--- resources/views/admin/development/exercises/data/show.blade.php
+++ resources/views/admin/development/exercises/data/show.blade.php
@@ -6,10 +6,9 @@
         @include('admin.components.audio.play', ['audio' => $data->content['audio']])
     @endisset
     {!! \App\Library\StrUtils::normalize(Arr::get($data->content, 'value')) !!}
-        <span>
+
     <splitter value="{{ Arr::get($data->content, 'value') }}"></splitter>
-            </span>
-        <span>
+
     @isset($data->content['extra_chunks'])
         <div class="h5">
         @foreach(preg_split('/,/', Arr::get($data->content, 'extra_chunks')) as $chunk)
@@ -19,5 +18,4 @@
             @endforeach
         @endisset
         </div>
-        </span>
 </div>
