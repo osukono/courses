fbca76214dd67b7999ffadacf9c628be5c9e53aa
Andrew Sukonchenko <osukono@gmail.com>
2021-06-30 10:14:07 +0300

    Commit

--- app/Http/Controllers/GrammarController.php
+++ app/Http/Controllers/GrammarController.php
@@ -11,7 +11,18 @@ class GrammarController extends Controller
         if ($courseLesson->grammar == null)
             abort(404);
 
+        $data['title'] = $courseLesson->title;
+        $data['previous'] = CourseLesson::where('course_id', '=', $courseLesson->course_id)
+            ->whereNotNull('grammar')
+            ->where('index', '<', $courseLesson->index)
+            ->orderBy('index', 'desc')
+            ->first();
         $data['grammar'] = $courseLesson->grammar;
+        $data['next'] = CourseLesson::where('course_id', '=', $courseLesson->course_id)
+            ->whereNotNull('grammar')
+            ->where('index', '>', $courseLesson->index)
+            ->orderBy('index')
+            ->first();
 
         return view('grammar')->with($data);
     }
--- resources/views/grammar.blade.php
+++ resources/views/grammar.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container justify-content-center">
-    {!! $grammar !!}
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                @isset($previous)
+                    <a href="{{ route('grammar', $previous) }}">&lsaquo; {{ $previous->title }}</a>
+                @endisset
+            </div>
+            <div class="col text-right">
+                @isset($next)
+                    <a href="{{ route('grammar', $next) }}">{{ $next->title }} &rsaquo;</a>
+                @endisset
+            </div>
+        </div>
+        <h2 class="text-center">{{ $title }}</h2>
+        <div class="my-4 mx-0 mx-md-3 mx-lg-5">
+            {!! $grammar !!}
+        </div>
     </div>
 @endsection
