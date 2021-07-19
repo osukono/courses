ee035d4073b2146a37e9aa416336158821482066
Andrew Sukonchenko <osukono@gmail.com>
2021-07-19 09:31:28 +0300

    Commit

--- resources/views/admin/languages/edit.blade.php
+++ resources/views/admin/languages/edit.blade.php
@@ -15,7 +15,7 @@
                 @input(['name' => 'native', 'label' => 'Native name', 'default' => $language->native])
                 @input(['name' => 'code', 'label' => 'Regional Code', 'default' => $language->code])
                 @input(['name' => 'locale', 'label' => 'Locale Code', 'default' => $language->locale])
-                @textarea(['name' => 'capitalized_words', 'label' => 'Capitalized Words', 'rows' => 5, 'default' => $language->capitalized_words, 'helper' => 'List capitalized words separating them with commas'])
+                @textarea(['name' => 'capitalized_words', 'label' => 'Capitalized Words', 'rows' => 5, 'default' => $language->capitalized_words, 'helper' => 'List of capitalized words separated with comma'])
                 @input(['name' => 'firebase_id', 'label' => 'Firebase ID', 'default' => $language->firebase_id])
 
                 @submit(['text' => 'Save'])
