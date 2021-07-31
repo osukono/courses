d1a0d0a39f5c59bdfede008c888984954700ecfc
Andrew Sukonchenko <osukono@gmail.com>
2021-07-31 17:15:30 +0300

    Commit

--- app/Library/Audio.php
+++ app/Library/Audio.php
@@ -9,10 +9,10 @@ class Audio
     public static function duration($audio) {
         $output = exec("sox " . $audio . " -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\).*$#\\1#p'");
 
-        $seconds = (int) substr($output, 0, strpos($output, '.'));
+        $seconds = intval(substr($output, 0, strpos($output, '.')));
         $milliseconds = substr($output, strpos($output, '.') + 1);
         $milliseconds = str_pad($milliseconds, 3, '0');
-        $milliseconds = (int) substr($milliseconds, 0, 3);
+        $milliseconds = intval(substr($milliseconds, 0, 3));
 
         return ($seconds * 1000) + $milliseconds;
     }
