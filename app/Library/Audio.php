5233731d037c71193a7a88a41262b5b834b5fa60
Andrew Sukonchenko <osukono@gmail.com>
2021-07-31 18:34:13 +0300

    Commit

--- app/Library/Audio.php
+++ app/Library/Audio.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Log;
 class Audio
 {
     public static function duration($audio) {
-        Log::debug(exec("sox " . $audio . " -n stat 2>&1"));
-        $output = exec("sox " . $audio . " -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\).*$#\\1#p'");
-        Log::debug($output);
+//        Log::debug(exec("sox " . $audio . " -n stat 2>&1"));
+        $output = exec("/home/linuxbrew/.linuxbrew/bin/sox " . $audio . " -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\).*$#\\1#p'");
+//        Log::debug($output);
 
         $seconds = intval(substr($output, 0, strpos($output, '.')));
         $milliseconds = substr($output, strpos($output, '.') + 1);
